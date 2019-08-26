cad9b5fcdd8b963c18483e264d548d5c051b1e9f
Irina Starshova <irina.starshova@accenture.com>
2019-08-26 13:23:11 +0300

    Добавлено логирование.

--- src/main/java/com/accenture/flowershop/be/access/DBEmulation.java
+++ src/main/java/com/accenture/flowershop/be/access/DBEmulation.java
@@ -2,12 +2,15 @@ package com.accenture.flowershop.be.access;
 
 import com.accenture.flowershop.be.entity.user.User;
 import com.accenture.flowershop.be.entity.user.UserImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 @Service
 public class DBEmulation implements UserAccessService{
     private  static Map<String, User> usersDB=new HashMap<>();
+    private static final Logger LOG = 	LoggerFactory.getLogger(UserAccessService.class);
     static {
         usersDB.put("admin",new UserImpl("admin", "admin123"));
         usersDB.put("John",new UserImpl("John", "123"));
@@ -22,7 +25,7 @@ public class DBEmulation implements UserAccessService{
         if(usersDB.containsKey(user.getName()))
             return false;
         usersDB.put(user.getName(),user);
-        System.out.println(usersDB);
+        LOG.debug("User with name = {} and password = {} was created!",user.getName(),user.getPassword());
         return true;
     }
 }
